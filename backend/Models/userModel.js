56accf41295ba2052fa3c559dff5e18dad00db7a
akshay2742 <akshays274@gmail.com>
2023-01-24 01:18:19 +0530

    configure the models for mongoDB

---
+++ backend/Models/userModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userModel = mongoose.Schema({
+    username:{type: String, required: true},
+    email:{type: String, required: true},
+    password:{type: String, required: true},
+    pic:{type: String,required: true, default:"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"},
+},{
+    timestamps: true
+});
