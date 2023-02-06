cc6d307f627125cce8b9566b168fa1b51d787cb9
Akshay Sharma <akshays274@gmail.com>
2023-02-06 18:56:48 +0530

    Added Signup Page

---
+++ frontend/src/Components/Authentication/Login.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Center,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import { FiChevronRight } from "react-icons/fi";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+
+  const changeEmailHandler = (e) => setEmail(e.target.value);
+
+  const isError = email === "";
+
+  //password
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <>
+      <FormControl>
+        <VStack spacing="4" align="start">
+          <FormLabel>Email address</FormLabel>
+          <Input
+            type="email"
+            value={email}
+            onChange={changeEmailHandler}
+            placeholder="Enter email address"
+          />
+          {!isError ? (
+            <FormHelperText>We'll never share your email.</FormHelperText>
+          ) : (
+            <FormErrorMessage color="red.300">
+              Email is required!.
+            </FormErrorMessage>
+          )}
+          <FormLabel>Password</FormLabel>
+          <InputGroup size="md">
+            <Input
+              pr="4.5rem"
+              type={show ? "text" : "password"}
+              placeholder="Enter password"
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handleClick}>
+                {show ? "Hide" : "Show"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          <Center w="100%" top="20px" position="relative">
+            <Button
+              rightIcon={<FiChevronRight />}
+              colorScheme="blue"
+              variant="solid"
+              marginLeft="auto"
+              marginRight="auto"
+            >
+              Login
+            </Button>
+          </Center>
+        </VStack>
+      </FormControl>
+    </>
+  );
+};
+
+export default Login;
