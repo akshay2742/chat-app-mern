4632325eb715f24297d3595dec3d6321cca167d8
Saad-Mohamed-Saad55 <161356330+Saad-Mohamed-Saad55@users.noreply.github.com>
2024-02-26 23:15:46 +0200

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect('mongodb+srv://Saad_Mohamed_Saad:z9NhzI8y3LmWhCmO@lms.wu8y1pe.mongodb.net/group_youssef?retryWrites=true&w=majority&appName=LMS', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
